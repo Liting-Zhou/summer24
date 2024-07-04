35c45130bcaf662dfbdc42a8aad0b2c23ca34d0d
Liting-Zhou <143493320+Liting-Zhou@users.noreply.github.com>
2024-07-04 14:48:58 -0700

    add TextInput and show anything user inputs

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 import Header from "./components/Header";
+import React, { useState } from "react";
 
 export default function App() {
   const appName = "Summer 2024 class";
+  const [text, setText] = useState("");
 
   return (
     <View style={styles.container}>
@@ -11,6 +13,12 @@ export default function App() {
         <Text>children1</Text>
         <Text>children2</Text>
       </Header>
+      <TextInput
+        value={text}
+        onChangeText={(newText) => setText(newText)}
+        placeholder="Enter something here..."
+      />
+      <Text>This is what you entered: {text}</Text>
       <StatusBar style="auto" />
     </View>
   );
